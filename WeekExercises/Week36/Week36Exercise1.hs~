72f1c8ab05b172ace7bf09f497d5d637c5456a71
Lars Martin Taraldset <Lars.Taraldset@student.uib.no>
2023-08-30 17:50:24 +0200

    Week36 exercise0 and 1 complete!

---
+++ WeekExercises/Week36/Week36Exercise1.hs~
@@ -0,0 +1,4 @@
+module Week36Exercise1 where
+
+f :: [Integer] -> [t] -> [(Integer, t)]
+f int ct = reverse $ zip int ct
---
+++ WeekExercises/Week36/Week36Exercise2.hs
@@ -0,0 +1,15 @@
+module Week36Exercise2 where
+
+--2a)
+
+semiRepetitive :: String -> Maybe String
+semiRepetitive s = 
+
+--2b)
+
+decomposeSemiRepetitive :: String -> Maybe (String, Maybe Char)
+
+--2c)
+
+CreateSemiRepetitive :: String -> Maybe Char -> String
+
