528a60ca6cd284c40aaee203f4ecd6dcebb4fdf6
Lars Martin Taraldset <Lars.Taraldset@student.uib.no>
2023-10-11 13:32:34 +0200

    Week 40 init

---
+++ WeekExercises/Week40/Week40Exercise0.hs
@@ -0,0 +1,7 @@
+module Week40Exercise0 where
+
+type SemiRepetitive = (String, Maybe Char)
+
+-- a) implement semiRepetitive
+semiRepetitive :: String -> Maybe SemiRepetitive
+semiRepetitive str = undefined
