8d3d4c4b6aa89f0d898ca5b639cf0e940addc7b2
Lars Martin Taraldset <Lars.Taraldset@student.uib.no>
2023-10-11 16:21:02 +0200

    Week40Exercise0 complete!

--- WeekExercises/Week40/Week40Exercise0.hs
+++ WeekExercises/Week40/Week40Exercise0.hs
@@ -2,6 +2,28 @@ module Week40Exercise0 where
 
 type SemiRepetitive = (String, Maybe Char)
 
--- a) implement semiRepetitive
+-- 1a) implement semiRepetitive
 semiRepetitive :: String -> Maybe SemiRepetitive
-semiRepetitive str = undefined
+semiRepetitive str =
+    let first = firstHalf str
+        second = secondHalf str
+        middleChar = findChar str
+    in if first == second then Just (first, middleChar) else Nothing
+
+findChar :: String -> Maybe Char
+findChar c
+    | odd (length c) = Just $ c !! max 0 (length (firstHalf c)) -- !! starts at 0
+    | otherwise = Nothing
+
+firstHalf :: String -> String
+firstHalf str = take (length str `div` 2) str
+
+secondHalf :: String -> String
+secondHalf str = drop ((length str + 1) `div` 2) str
+
+-- 1b) implement toString
+toString :: SemiRepetitive -> String
+toString (a, b) = case b of
+    Nothing -> a ++ a
+    Just b -> a ++ [b] ++ a
+    
