b6f9f1b85da9a17cec4d058931f64901ffcae80f
Lars Martin Taraldset <Lars.Taraldset@student.uib.no>
2023-10-11 18:45:58 +0200

    Week40Exercise2 complete !

--- WeekExercises/Week40/Week40Exercise2.hs
+++ WeekExercises/Week40/Week40Exercise2.hs
@@ -1,6 +1,18 @@
 module Week40Exercise2 where
+import Data.List (sort)
 
 data BinSearchTree a
   = Empty
   | Branch (BinSearchTree a) a (BinSearchTree a)
   deriving (Eq, Show)
+
+  -- implement toBinarySearchTree
+toBinarySearchTree :: Ord a => [a] -> BinSearchTree a
+toBinarySearchTree [] = Empty
+toBinarySearchTree list =
+  let sortedList = sort list
+      rootIndex = length sortedList `div` 2
+      lt = take rootIndex sortedList
+      rt = drop (rootIndex + 1) sortedList
+      root = sortedList !! rootIndex
+  in Branch (toBinarySearchTree lt) root (toBinarySearchTree rt)
