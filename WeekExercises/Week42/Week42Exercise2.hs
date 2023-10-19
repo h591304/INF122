2c0b8c0125d2e1290b0158848de3665ba8fbe281
Lars Martin Taraldset <Lars.Taraldset@student.uib.no>
2023-10-19 17:22:58 +0200

    Week42 passes all tests

--- WeekExercises/Week42/Week42Exercise2.hs
+++ WeekExercises/Week42/Week42Exercise2.hs
@@ -2,7 +2,7 @@ module Week42Exercise2 where
 
 -- a) implement ifFiveMultiples point-free
 isFiveMultiples :: [Integer] -> Bool
-isFiveMultiples = all (\x -> x `mod` 5 == 0) 
+isFiveMultiples = all $ \x -> x `mod` 5 == 0 
 
 -- b) implement factorial
 factorial :: Integer -> Integer
